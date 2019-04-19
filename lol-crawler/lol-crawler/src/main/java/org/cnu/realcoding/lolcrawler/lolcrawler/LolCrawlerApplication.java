3cd85ad862c74d06ec3285038fc85c71184d4d98
solidw <solidw@naver.com>
2019-04-20 01:47:23 +0900

    chore : Initailizing spring boot (web) project

---
+++ lol-crawler/lol-crawler/src/main/java/org/cnu/realcoding/lolcrawler/lolcrawler/LolCrawlerApplication.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding.lolcrawler.lolcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolCrawlerApplication.class, args);
+	}
+
+}
---
+++ lol-crawler/lol-crawler/src/test/java/org/cnu/realcoding/lolcrawler/lolcrawler/LolCrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package org.cnu.realcoding.lolcrawler.lolcrawler;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LolCrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
