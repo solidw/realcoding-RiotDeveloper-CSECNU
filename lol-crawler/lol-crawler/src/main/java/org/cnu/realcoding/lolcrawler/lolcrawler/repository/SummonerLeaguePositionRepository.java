8f1fda8bea443f38f033a2eedee3a377519e8e49
solidw <solidw@naver.com>
2019-04-20 15:39:15 +0900

    feat : Add MongoDB feature but 50% done :)..

---
+++ lol-crawler/lol-crawler/src/main/java/org/cnu/realcoding/lolcrawler/lolcrawler/repository/SummonerLeaguePositionRepository.java
@@ -0,0 +1,19 @@
+package org.cnu.realcoding.lolcrawler.lolcrawler.repository;
+
+import org.cnu.realcoding.lolcrawler.lolcrawler.domain.LeaguePositionEncryptedID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SummonerLeaguePositionRepository {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public void insertLeaguePosition(LeaguePositionEncryptedID leaguePositionEncryptedID) {
+        mongoTemplate.insert(leaguePositionEncryptedID);
+    }
+
+
+}
