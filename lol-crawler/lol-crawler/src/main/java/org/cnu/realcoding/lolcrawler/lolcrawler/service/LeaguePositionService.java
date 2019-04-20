c4035b88b84ccd13b124fd47a0e99c4514cacf49
solidw <solidw@naver.com>
2019-04-20 15:38:47 +0900

    temp : Add temp data I'll fixed after :)

---
+++ lol-crawler/lol-crawler/src/main/java/org/cnu/realcoding/lolcrawler/lolcrawler/service/LeaguePositionService.java
@@ -0,0 +1,11 @@
+package org.cnu.realcoding.lolcrawler.lolcrawler.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LeaguePositionService {
+
+//    @Autowired
+//    private
+}
